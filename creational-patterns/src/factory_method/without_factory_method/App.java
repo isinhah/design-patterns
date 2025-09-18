b5cacf55eee213a5756eb4e1dd2c30f852d602ad
Isabel Henrique <isabel.henrique.dev@gmail.com>
2025-09-18 10:00:59 -0300

    exemplo de código sem usar o factory method

---
+++ creational-patterns/src/factory_method/without_factory_method/App.java
@@ -0,0 +1,16 @@
+package factory_method.without_factory_method;
+
+import model.IPhone;
+import factory_method.without_factory_method.service.IphoneService;
+
+public class App {
+    public static void main(String[] args) {
+        IphoneService iphoneService = new IphoneService();
+
+        IPhone iphone = iphoneService.orderIPhone("X", "standard", "black");
+        System.out.println(iphone);
+
+        IPhone iphone2 = iphoneService.orderIPhone("11", "highEnd", "white");
+        System.out.println(iphone2);
+    }
+}
